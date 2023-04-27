1504fee88f6e3701829cb35bb4133332804fc516
PashaSeleznev <113536910+PashaSeleznev@users.noreply.github.com>
2023-04-27 18:32:12 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
+import pyttsx3, pyaudio, vosk, requests
 import json, os
 
-import pyttsx3, pyaudio, vosk
-
-tts = pyttsx3.init('sapi5')
+tts = pyttsx3.init()
 
 voices = tts.getProperty('voices')
 tts.setProperty('voices', 'en')
 
 for voice in voices:
-    print(voice.name)
-    if voice.name == 'Microsoft Zira Desktop - English (United States)':
-        tts.setProperty('voice', voice.id)
+    if voice.name == 'Microsoft David Desktop - English (United States)':
+        tts.setProperty('voices', voice.id)
 
 model = vosk.Model('model_small')
 record = vosk.KaldiRecognizer(model, 16000)
-pa = pyaudio.PyAudio()
-stream = pa.open(format=pyaudio.paInt16,
-                 channels=1,
-                 rate=16000,
-                 input=True,
-                 frames_per_buffer=8000)
+aud = pyaudio.PyAudio()
+stream = aud.open(format=pyaudio.paInt16,
+                  channels=1,
+                  rate=16000,
+                  input=True,
+                  frames_per_buffer=8000)
 stream.start_stream()
 
 
-def listen():
+def listening():
     while True:
         data = stream.read(4000, exception_on_overflow=False)
         if record.AcceptWaveform(data) and len(data) > 0:
@@ -32,17 +30,50 @@ def listen():
                 yield answer['text']
 
 
-def speak(say):
+def speaking(say):
     tts.say(say)
     tts.runAndWait()
 
 
-speak('starting')
-print('start...')
-for text in listen():
+speaking('Starting')
+print("Let's go...")
+
+main_data = []
+commands = {'занятие', 'тип', 'участники', 'стоимость', 'закрыть'}
+for text in listening():
+    print(text)
+    if text == 'занятие':
+        main_data = []
+        res = requests.get("https://www.boredapi.com/api/activity")
+        data = res.json()
+        main_data = data
+        print(main_data['activity'])
+        speaking(str(main_data['activity']))
+
+    if text == 'тип':
+        if main_data != []:
+            print(main_data['type'])
+            speaking(str(main_data['type']))
+        else:
+            speaking('I do not know what action you are talking about')
+
+    if text == 'участники':
+        if main_data != []:
+            print(main_data['participants'])
+            speaking(str(main_data['participants']))
+        else:
+            speaking('I do not know what action you are talking about')
+
+    if text == 'стоимость':
+        if main_data != []:
+            print(main_data['price'], 'dollars')
+            speaking(str(main_data['price'])+'dollars')
+        else:
+            speaking('I do not know what action you are talking about')
+
     if text == 'закрыть':
         quit()
-    elif text == 'блокнот':
-        os.system('notepad.exe')
-    else:
-        print(text)
+
+    if text not in commands:
+        speaking('I have not learnt this word yet:(')
+
